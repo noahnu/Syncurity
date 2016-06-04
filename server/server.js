4306c4bace337ce44c02e93603a84e1b4565e610
splacorn <curtischong5@gmail.com>
2016-06-04 10:38:58 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 var mqtt = require('mqtt');
+var base64 = require('node-base64-image');
+
 
 var PUBLISH_TOPIC = "iot-2/evt/pingFrom/fmt/json";
 var SUBSCRIBE_TOPIC = "iot-2/cmd/pingTo/fmt/json";
@@ -10,6 +12,18 @@ var type = "computer";
 var deviceId = "123456789"
 var username = 'use-token-auth';
 var password = "47@*BY8!CrMmM_3)2o"
+
+var testImage;
+
+var path = '../curtischong.jpg',
+options = {localFile: true, string: true};
+base64.base64encoder(path, options, function (err, image) {  
+    if (err) { console.log(err); }  
+    testImage = image;
+}); 
+
+
+
 
 var clientId = "d:" + org + ":" + type + ":" + deviceId;
 console.log('MQTT clientId = ' + clientId);
@@ -25,6 +39,17 @@ mqttClient = mqtt.connect("mqtts://" + org + ".messaging.internetofthings.ibmclo
    password : password
 });
 
+
+
+fs.readFile(image_origial, function(err, original_data){
+    fs.writeFile('image_orig.jpg', original_data, function(err) {});
+    var base64Image = original_data.toString('base64');
+    var decodedImage = new Buffer(base64Image, 'base64');
+    fs.writeFile('image_decoded.jpg', decodedImage, function(err) {});
+});
+
+
+
 mqttClient.on('connect', function()
 {
    console.log('MQTT Connected.');
@@ -36,6 +61,7 @@ mqttClient.on('connect', function()
 
    var data={};
    data.message="Hello World!";
+    data.photo = testImage;
 
    mqttClient.publish(PUBLISH_TOPIC, JSON.stringify(data));
 });
