f88734bdd65bbcf8fcdeb88ac045ca31a39d3d18
Noah NU <noahnu@gmail.com>
2016-06-04 19:36:18 -0400

    Refactored server.js. Identifies Curtis.

--- server/server.js
+++ server/server.js
@@ -1,76 +1,85 @@
+"use strict";
+
 var mqtt = require('mqtt');
 var fs = require('fs');
 var xml = require('xmlhttprequest');
 
-var PUBLISH_TOPIC = "iot-2/evt/pingFrom/fmt/json";
-var SUBSCRIBE_TOPIC = "iot-2/cmd/pingTo/fmt/json";
-
-var mqttClient;
-
-var org = "ziwbp7";
-var type = "computer";
-var deviceId = "123456789";
-var username = 'use-token-auth';
-var password = "47@*BY8!CrMmM_3)2o";
-
-var path = '../curtischong.jpg';
-//var testImage = base64_encode(path);
-//console.log(testImage);
-
-// function to encode file data to base64 encoded string
-function base64_encode(file) {
-    // read binary data
-    var bitmap = fs.readFileSync(file);
-    // convert binary data to base64 encoded string
-    return new Buffer(bitmap).toString('base64');
-}
-
-
-
-var clientId = "d:" + org + ":" + type + ":" + deviceId;
-console.log('MQTT clientId = ' + clientId);
-
-
-console.log('Waiting for MQTT Connection...');
-//mqttClient = mqtt.connect("mqtts://" + org + ".messaging.internetofthings.ibmcloud.com:8883",
-mqttClient = mqtt.connect("mqtts://" + org + ".messaging.internetofthings.ibmcloud.com:8883",
-{
-   clientId : clientId,
-   keepalive : 120,
-   username : username,
-   password : password
+var watson = require('watson-developer-cloud');
+var visual_recognition = watson.visual_recognition({
+	api_key: '2ee99e97dc6199948ff0611bf9f03a52cdc90183',
+	version_date: '2016-05-20',
+	version: 'v3'
 });
 
-function readImage (fileName) {
-	return fs.readFileSync(fileName);
-}
+var bluemix = {
+	client: undefined,
+	
+	publish: {
+		ping: "iot-2/evt/pingFrom/fmt/json"
+	},
+	
+	subscribe: {
+		ping: "iot-2/cmd/pingTo/fmt/json"
+	},
+	
+	opts: {
+		org: "ziwbp7",
+		type: "computer",
+		deviceId: "123456789",
+		username: 'use-token-auth',
+		password: "47@*BY8!CrMmM_3)2o",
+		clientId: ""
+	},
+	
+	getUrl: () => {
+		return "mqtts://" + bluemix.opts.org + ".messaging.internetofthings.ibmcloud.com:8883";
+	},
+	
+	events: {
+		connected: () => {
+			console.log("Connected to bluemix.");
+			
+			bluemix.client.subscribe(bluemix.subscribe.ping, () => {
+				bluemix.client.on('message', (topic, message, packet) => {
+					console.log("Received '" + message + "' on '" + topic + "'");
+				});
+			});
+			
+			core.classify(fs.createReadStream('curtischong.jpg'));
+			
+			//bluemix.client.publish(bluemix.publish.ping, JSON.stringify(msg));
+		}	
+	}
+};
 
-mqttClient.on('connect', function()
-{
-   console.log('MQTT Connected.');
+var core = {
+	init: () => {
+		bluemix.opts.clientId = "d:" + bluemix.opts.org + ":" + bluemix.opts.type + ":" + bluemix.opts.deviceId;
+		
+		console.log("Syncur.io starting...");
+		
+		bluemix.client = mqtt.connect(bluemix.getUrl(), {
+			clientId: bluemix.opts.clientId,
+			keepalive: 120,
+			username: bluemix.opts.username,
+			password: bluemix.opts.password
+		});
+		
+		bluemix.client.on('connect', bluemix.events.connected);
+	},
+	
+	classify: (stream) => {
+		visual_recognition.classify({
+			images_file: stream
+		}, function(err, res){
+			if (err) {
+				console.log('Failed to communicate with API: ');
+				console.log(err);
+			} else {
+				console.log(JSON.stringify(res, null, 2));
+			}
+		});
+	}
+};
 
-   mqttClient.subscribe(SUBSCRIBE_TOPIC, function()
-   {
-      mqttClient.on('message', onMessage);
-   });
-
-	var data = { payload: {image: ''} };
-  	data.payload.image = ""+testImage;//readImage("../curtischong.jpg");
-	console.log(data);
-
-   mqttClient.publish(PUBLISH_TOPIC, JSON.stringify(data));
-});
-
-
-function onMessage(topic, message, packet)
-{
-   console.log("Received '" + message + "' on '" + topic + "'");
-}
-
-
-function pause()
-{
-   setTimeout(pause,5000);
-}
-
-pause();
+core.init();
