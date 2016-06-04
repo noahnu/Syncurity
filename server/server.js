52fd5a2992410af98e010f1a2e061ff22aabc537
splacorn <curtischong5@gmail.com>
2016-06-04 11:02:39 -0400

    got base 64 working

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var mqtt = require('mqtt');
-var base64 = require('node-base64-image');
-
+var fs = require('fs');
 
 var PUBLISH_TOPIC = "iot-2/evt/pingFrom/fmt/json";
 var SUBSCRIBE_TOPIC = "iot-2/cmd/pingTo/fmt/json";
@@ -13,15 +12,17 @@ var deviceId = "123456789"
 var username = 'use-token-auth';
 var password = "47@*BY8!CrMmM_3)2o"
 
-var testImage;
+var path = '../curtischong.jpg';
+var testImage = base64_encode(path);
 
-var path = '../curtischong.jpg',
-options = {localFile: true, string: true};
-base64.base64encoder(path, options, function (err, image) {  
-    if (err) { console.log(err); }  
-    testImage = image;
-}); 
 
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
 
 
 
@@ -39,7 +40,7 @@ mqttClient = mqtt.connect("mqtts://" + org + ".messaging.internetofthings.ibmclo
    password : password
 });
 
-
+/*
 
 fs.readFile(image_origial, function(err, original_data){
     fs.writeFile('image_orig.jpg', original_data, function(err) {});
@@ -47,7 +48,7 @@ fs.readFile(image_origial, function(err, original_data){
     var decodedImage = new Buffer(base64Image, 'base64');
     fs.writeFile('image_decoded.jpg', decodedImage, function(err) {});
 });
-
+*/
 
 
 mqttClient.on('connect', function()
